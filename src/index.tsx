be487392589e4b86ac90d7f3f0325bb17e67f6e0
gdradi <giacomo.dradi@gmail.com>
2022-03-09 21:22:07 +0100

    Boot app

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+let a = 1;
+let b = 2;
+let c = a+b;
+console.log(c);
+
+
+export {};
+// import ReactDOM from 'react-dom';
+// import './index.scss';
+
+// ReactDOM.render(
+//   <div>Hello world from FITSTIC - React app!</div>,
+//   document.getElementById('root')
+// );
